6dabb5195b082abba907fc23bc95fa8b3a96a1e5
Jenny Carroll <jennygc@telus.net>
2023-02-14 23:41:02 +0000

    create function timer that takes in arguments from the command line and outputs a sound for each interval in seconds specified by the argument

--- timer1.js
+++ timer1.js
@@ -1,8 +1,19 @@
 const args = process.argv.slice(2);
 
+// const args = [1, 3, 6];
+
 const timer = function (args) {
-  process.stdout.write("\x07");
-}
+  let time = 0;
+  for (let arg of args) {
+    if (arg < 0 || isNaN(arg)) {
+      return;
+    }
+    time = arg * 1000;
+    setTimeout(() => process.stdout.write("\x07"), time);
+  }
+};
+
+timer(args);
 
 // No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
 // An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
